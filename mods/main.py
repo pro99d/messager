6627ca54f15d8644b64f5577d2f7c353355d4796
user <pro999999d@gmail.com>
2026-05-10 20:30:21 +0300

    wip on server and ui

--- mods/main.py
+++ mods/main.py
@@ -5,6 +5,7 @@ import threading
 from threading import Thread
 from socket_for_humans import Server, Connection
 import json
+import time
 
 @dataclass
 class Config:
@@ -12,6 +13,31 @@ class Config:
     ui: module.Module
     encryption: module.Module
     application: module.Module
+
+class Chats:
+    """
+    сюда БДшку
+    """
+    def __init__(self) -> None:
+        self.messages = []
+
+    def add_message(self, message: str, sender: str, reciver: str):
+        self.messages.append({
+            "sender": sender,
+            "reciver": reciver,
+            "msg": message,
+            "time": time.time()
+        })
+
+    def update_messages(self, messages: list[dict]) -> None:
+        self.messages = list(set(self.messages) | set(messages))
+
+    def filter_by_access(self, ip: str) -> list[dict]:
+        result = []
+        for message in self.messages:
+            if ip in (message["sender"], message["reciver"]):
+                result.append(message)
+        return result
 
 class Application(module.Module):
     def __init__(self) -> None:
@@ -30,6 +56,7 @@ class Encryption(module.Encryption):
 class UI(module.UI):
     def __init__(self) -> None:
         super().__init__(module_name= "MainUI")
+        self.chats = Chats()
 
     def start_ui(self):
         pass
@@ -42,6 +69,8 @@ class Network(module.Network):
         super().__init__(module_name= "MainNet")
         self.server_running = True
         addr = ('0.0.0.0', SERVER_PORT)
+        self.active_connections = set()
+
     def init_network(self, encryption: module.Encryption, ui: module.UI) -> None:
         self.enc = encryption
         self.ui = ui
@@ -65,18 +94,38 @@ class Network(module.Network):
     def start_server(self) -> None:
         while True:
             msg, conn, addr = self.server.get_next()
-            src = addr[0]
+            sender_ip = addr[0]
             msg = self.enc.decrypt(msg)
             formated = json.loads(msg)
-            if formated["type"] == "message":
-                self.ui.on_message_recive(formated["content"], src)
-            elif formated["type"] == "quit":
-                break
+            match formated["type"]:
+                case "message":
+                    self.ui.on_message_recive(formated["content"], sender_ip)
+                case "join_message":
+                    self.active_connections.add(sender_ip)
+                    if not hasattr(self.ui, "chats"):
+                        continue
+                    allowed_messages = self.ui.chats.filter_by_access(sender_ip)
+                    self.send_msg(json.dumps(allowed_messages), sender_ip, "chats")
+                case "chats":
+                    if not hasattr(self.ui, "chats"):
+                        continue
+                    messages = json.loads(formated["content"])
+                    self.ui.chats.update_messages(messages)
+                case "quit_message":
+                    self.active_connections.remove(sender_ip)
+                case "quit":
+                    break
+            # if formated["type"] == "message":
+            # elif formated["type"] == "quit":
+            #     break
+            # elif formated["type"]
             
     def stop_server(self) -> None:
         if self.server_running:
             self.send_msg("", "127.0.0.1", "quit")
             self.server_running = False
+            if len(self.active_connections) > 0:
+                self.send_msg("", list(self.active_connections)[0], "quit_message")
     
 
 class ConfigParser(module.ConfigParser):
